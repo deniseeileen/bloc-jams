062bacd50b0d11f36cac8f7b76b4075b5f4b5d6c
Denise Martin <denise.e.martin@gmail.com>
2017-08-26 20:05:16 -0400

    Checkpoint 22 Events: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
  
      var revealPoint = function(){
          document.querySelectorAll("points");
@@ -17,3 +17,19 @@ var animatePoints = function() {
      revealPoint();
  
  };
+
+window.onload = function() {
+    
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
